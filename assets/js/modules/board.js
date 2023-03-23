7104398cdc332edc6479103576fce86a1a0b3c46
devs.hashtag <devs.hashtag@gmail.com>
2023-03-23 16:54:01 +0330

    modules/board updated

--- assets/js/modules/board.js
+++ assets/js/modules/board.js
@@ -1,128 +1,128 @@
-import { BOARD_SIZE, STYLES, CLASS_NAMES } from "./settings.js";
+import { STYLES, CLASS_NAMES, INIT_TURN } from "./settings.js";
 
 function Board() {
-  this.boardSize = BOARD_SIZE;
+  this.styles = STYLES;
+  this.classes = CLASS_NAMES;
+  this.initTurn = INIT_TURN;
+  this.elements = [];
+  this.cells = [];
 
-  this.blocks = [];
+  this.init = function () {
+    // root
+    this.elements.root = document.querySelector("body > main");
 
-  this.elements = {
-    root: document.querySelector("body > main"),
-  };
-
-  this.init = function (initTurn) {
     // container
     this.elements.container = this.addContainer();
 
     // board
-    this.elements.board = this.addBoard(initTurn);
+    this.elements.board = this.addBoard();
 
-    // add board blocks
-    this.addBlocks();
+    // add board cells
+    this.addCells();
   };
 
   this.addContainer = function (parent = this.elements.root) {
-    const container = this.addElement(CLASS_NAMES.container, parent);
+    const container = this.addElement(this.classes.container, parent);
 
     container.textContent = "tic tac toe";
 
     return container;
   };
 
-  this.addBoard = function (initTurn) {
-    const board = this.addElement(CLASS_NAMES.board, this.elements.container);
+  this.addBoard = function (parent = this.elements.container) {
+    const styles = this.styles.board;
+    const board = this.addElement(this.classes.board, parent);
 
-    // board grid-gap, padding, grid-template-columns
-    board.style.gap = STYLES.board.gap + "rem";
-    board.style.borderWidth = STYLES.board.width + "rem";
-    board.style.borderRadius = STYLES.board.radius + "rem";
-    board.style.padding = STYLES.board.padding + "rem";
-    board.style.gridTemplateColumns = `repeat(${this.boardSize}, 1fr)`;
-
-    // set board init turn
-    this.setClassName(board, initTurn);
+    // board styles
+    Object.assign(board.style, {
+      gap: styles.gap,
+      padding: styles.padding,
+      borderWidth: styles.borderWidth,
+      borderRadius: styles.borderRadius,
+      gridTemplateColumns: `repeat(${styles.cells}, 1fr)`,
+    });
 
     return board;
   };
 
-  this.addBlocks = function () {
-    for (let i = 0; i < this.boardSize ** 2; i++) {
-      this.addBlock();
-    }
+  this.addCell = function () {
+    const styles = this.styles.cell;
+    const cell = this.addElement(this.classes.cell);
+
+    // styles
+    Object.assign(cell.style, {
+      width: styles.size,
+      height: styles.size,
+      borderRadius: styles.borderRadius,
+    });
+
+    // add cell to cells list
+    this.cells.push(cell);
+
+    return cell;
   };
 
-  this.addBlock = function () {
-    const block = this.addElement(CLASS_NAMES.block);
-
-    // block size
-    const size = STYLES.block.size + "rem";
-    block.style.width = size;
-    block.style.height = size;
-
-    // border radius
-    block.style.borderRadius = STYLES.block.radius + "rem";
-
-    // add block to blocks
-    this.blocks.push(block);
-
-    return block;
+  this.addCells = function () {
+    for (let i = 1; i <= this.styles.board.cells ** 2; i++) {
+      this.addCell();
+    }
   };
 
   this.addElement = function (className, parent = this.elements.board) {
     const element = document.createElement("div");
 
+    // class name
     this.setClassName(element, className);
 
+    // append element to parent
     parent.appendChild(element);
 
     return element;
   };
 
   this.setClassName = function (element, className) {
-    if (typeof className == "string") {
-      element.classList.add(className);
-    } else {
-      element.classList.add(...className);
-    }
+    if (typeof className == "string") element.classList.add(className);
+    else element.classList.add(...className);
   };
 
-  this.removeClassName = function (element, className) {
-    element.classList.remove(className);
+  // get current turn
+  this.getTurn = function (element = this.elements.board) {
+    return element.className.split(" ").at(-1);
   };
 
-  this.getBlockTurn = function (block = this.elements.board) {
-    const boardTurn = block.className.split(" ").pop();
-    const blockTurn = boardTurn.split("-")[0];
-
-    return blockTurn;
+  this.setTurn = function (turn = this.initTurn) {
+    this.elements.board.classList.add(turn);
   };
 
-  this.setBlockTurn = function (block) {
-    // get current block turn
-    const blockTurn = this.getBlockTurn();
+  // remove current board turn
+  this.removeTurn = function () {
+    const turn = this.getTurn();
 
-    // set turn to block
-    this.setClassName(block, blockTurn);
+    this.elements.board.classList.remove(turn);
   };
 
-  this.noTurn = function () {
-    const board = this.elements.board;
+  // toggle turns
+  this.toggleTurns = function () {
+    const boardTurns = Object.values(this.classes.boardTurns);
 
-    board.classList.remove(CLASS_NAMES.boardTurns.o);
-    board.classList.remove(CLASS_NAMES.boardTurns.x);
+    boardTurns.forEach((turn) => {
+      this.elements.board.classList.toggle(turn);
+    });
   };
 
-  this.changeTurn = function () {
-    const board = this.elements.board;
+  // get current cell turn
+  this.getCellTurn = function (element = this.elements.board) {
+    const boardTurn = element.className.split(" ").at(-1);
+    const cellTurn = boardTurn.split("-")[0];
 
-    board.classList.toggle(CLASS_NAMES.boardTurns.o);
-    board.classList.toggle(CLASS_NAMES.boardTurns.x);
+    return cellTurn;
   };
 
-  this.blockPosition = function (block) {
-    return {
-      x: parseInt(block.style.left) / this.block.size,
-      y: parseInt(block.style.top) / this.block.size,
-    };
+  this.setCellTurn = function (cell) {
+    const turn = this.getCellTurn();
+
+    // set turn to cell
+    this.setClassName(cell, turn);
   };
 }
 
